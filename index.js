a7c2e06e896954e88b0b954849e144fb364db747
Samantha Morco <samanthamorco@gmail.com>
2017-03-15 18:59:20 -0400

    edit index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* global moment */
-var moment = require('moment');
+var moment = require("moment");
 console.log("Hello Everyone!");
-var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+var datetime = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(datetime);
